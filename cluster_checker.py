42c5f1d08cd9f94ffafd29461270f2a504471f00
V4N15H <ikozadaev2018@gmail.com>
2025-11-13 15:36:22 +0400

    initial commit

---
+++ cluster_checker.py
@@ -0,0 +1,168 @@
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import httpx
+from datetime import datetime, timedelta, timezone
+
+
+# ============ 1. Настройки ============
+load_dotenv()
+SITE_API_URL = "https://seller.ozon.ru/api/site/fbo-posting-service/posting/list"  # URL для парсинга заказов
+API_URL = "https://api-seller.ozon.ru/v2/posting/fbo/list"
+OUTPUT_FILE = "ozon_clusters.xlsx"
+
+
+# ============ 2. Функции ============
+def get_posting_numbers():
+    """
+    Парсит сайт заказов и извлекает номера отправлений (posting_number),
+    где время доставки = 28 часов.
+    """
+    timeout = httpx.Timeout(connect=5.0, read=40.0, write=5.0, pool=5.0)
+    client = httpx.Client(timeout=timeout)
+
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "ru",
+        "Content-Type": "application/json",
+        "Cookie":"__Secure-ext_xcid=27faf522100f77d2b130d016cfc1b9a7; __Secure-ab-group=38; __Secure-ETC=bc0825e8423099a1cb9076432fb3f7f1; ADDRESSBOOKBAR_WEB_CLARIFICATION=1762859777; __Secure-user-id=142345068; bacntid=6248116; sc_company_id=290929; x-o3-language=ru; abt_data=7.m1MvWyNUHGd8Y96nZKoBJVJ7-GOTXw8nZqFDLhssPNnDHSFm0fb3qJInkCG1fNafBVVPhbI3XdC45DZ3UrUSSOzDQG6GElxIcRXtJ4J2ri3obeB3JPjNl2sBq7Pw1rj_Rpq-tsh5H7foB0F4wOpxDupAov-JJUtyNyOZEarUSbAnP1aNza7ZriOcWbFuQQ1U-6qAUuSH2_FPERRBJlNpVUkBYdHkeptSSsTLzIdzxGjfFP-jXI2EZjVa-7Zx68OY6dBl4I_Spr1qb0Y62d7To56TD11nFAHNGOloao7dc8uyLyrba9kkHXpKfhBsW5K4Ty7ZqiC1xvT5cHv7olJlnIpcB0MDnlamrIkqFiHHqZZSgDEwIlepPRQhGddDh6wXEogCWRv4V0Ejuunqnxn3mSq0MK0OV5-0shcCWPocNXNNr4gOGlyoKMsyTufJJ4JPkETNWs8pGVEUq3kYRRnDjhVAis1DzeV7lyN0RGDzVwmZMaaH97Tt1xBzxtxEunEuyGatOJ8U9ktPbqhsAXUR5amTEq1HDTmuCEhVkCi2OLD4SZ9KKgnNdvj00Kh5EVwZ74ud39N3FMNOob6xKC_14mCzNm_6DrVhvYnP0TE0OJNrPRlAumzwJfm1T3b2f5FevFyjXso1NDpST1I; __Secure-refresh-token=9.142345068.VbGj-Qd2SeOQJIO8YLXtXA.38.AUCdFbPi2a1F3PmVoH7kI3arqE4n5PnS4DY4PD3U62nP7LmLvIEzORk9i0t0D8xsCDvqbpXQyo5-G1iOt8dcOk8BAuIe_GAWTXXTjay3ZFt3DAAs6VfHw9P0Sx1lmV6fOA.20230917093549.20251112113145.LHWp50chgOz1rz8e6j4jB2vRt98SOSuusKF58izRpaM.1fb9d0a13a6ef9250; __Secure-access-token=9.142345068.VbGj-Qd2SeOQJIO8YLXtXA.38.AUCdFbPi2a1F3PmVoH7kI3arqE4n5PnS4DY4PD3U62nP7LmLvIEzORk9i0t0D8xsCDvqbpXQyo5-G1iOt8dcOk8BAuIe_GAWTXXTjay3ZFt3DAAs6VfHw9P0Sx1lmV6fOA.20230917093549.20251112113145.rmT1OgACAx9Epy7cPuKgrwhbIdUrGU2oSGJa5vsAJrI.12e51e3635b58e9fd",
+        "Origin": "https://seller.ozon.ru",
+        "Priority": "u=1, i",
+        "Referer": "https://seller.ozon.ru/app/postings/fbo?statusAlias=all&processedAtFrom=2025-11-10T20%3A00%3A00.000Z&processedAtTo=2025-11-11T19%3A59%3A59.999Z",
+        "Sec-Ch-Ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+        "X-O3-App-Name": "seller-ui",
+        "x-O3-Company-Id": "290929",
+        "x-O3-Language": "ru",
+        "x-O3-Page-Type": "PostingsFbo",
+    }
+    cursor = None
+    posting_numbers = []
+    counter = 0
+    while True:
+        json_payload = {
+        "filter": {
+            "company_id": "290929",
+            "processed_at_from": (datetime.now(timezone.utc) - timedelta(days=3))
+            .isoformat(timespec="seconds")
+            .replace("+00:00", "Z"),
+            "processed_at_to": datetime.now(timezone.utc)
+            .isoformat(timespec="seconds")
+            .replace("+00:00", "Z"),
+        },
+        "limit": "1000",
+        "sort_dir": "desc",
+        }
+        if cursor:
+            json_payload["cursor"] = cursor
+        try:
+            response = client.post(url=SITE_API_URL, headers=headers, json=json_payload, follow_redirects=True)
+            data = response.json()
+            result = data.get("result")
+            cursor = result.get("cursor")
+            postings = result.get("postings")
+            for p in postings:
+                est_time = p.get("estimated_delivery_time_hours")
+                # Фильтр по времени доставки (точное совпадение)
+                if est_time != "28":
+                        continue
+                posting_number = p.get("posting_number")
+                posting_numbers.append(posting_number)
+                counter +=1
+            if not cursor or cursor == "":
+                break
+        except httpx.ReadTimeout:
+            print("Время ожидания вышло, прекращаю цикл")
+            break
+    return posting_numbers
+
+
+def get_api_data(posting_numbers):
+    """
+    Получает данные о городe, складe и связанных кластерах через API.
+    """
+    headers = {
+        "Host": "api-seller.ozon.ru",
+        "Client-Id": os.environ.get("CLIENT_ID"),
+        "Api-Key": os.environ.get("API_KEY"),
+        "Content-Type": "application/json",
+    }
+    json_payload = {
+        "dir": "DESC",
+        "filter": {
+            "since": (datetime.now(timezone.utc) - timedelta(days=3))
+            .isoformat(timespec="seconds")
+            .replace("+00:00", "Z"),
+            "to": datetime.now(timezone.utc)
+            .isoformat(timespec="seconds")
+            .replace("+00:00", "Z"),
+        },
+        "limit": 1000,
+        "offset": 0,
+        "translit": True,
+        "with": {
+            "analytics_data": True,
+            "financial_data": True,
+        },
+    }
+    results = []
+    seen_postings = []
+    counter = 0
+    while True:
+        response = requests.post(url=API_URL, headers=headers, json=json_payload)
+        data = response.json()
+        result = data.get("result", [])
+        if not result or result == []:
+            print("Достигнут предел пагинации")
+            break
+        for item in result:
+            posting_number = item.get("posting_number")
+            if posting_number in posting_numbers: #and posting_number not in seen_postings:
+                seen_postings.append(posting_number)
+                analytics = item.get("analytics_data", {})
+                finances = item.get("financial_data", {})
+                results.append(
+                    {
+                        "posting_number": posting_number,
+                        "order_id": item.get("order_id"),
+                        "city": analytics.get("city"),
+                        "warehouse_id": analytics.get("warehouse_id"),
+                        "warehouse_name": analytics.get("warehouse_name"),
+                        "cluster_from": finances.get("cluster_from"),
+                        "cluster_to": finances.get("cluster_to"),
+                    }
+                )
+        counter += 1
+        json_payload["offset"] = 1000 * counter
+    return results
+
+
+def save_to_excel(data, filename):
+    """
+    Сохраняет результаты в Excel.
+    """
+    df = pd.DataFrame(data)
+    df.to_excel(filename, index=False)
+    print(f"Результаты сохранены в {filename}")
+
+
+# ============ 3. Основная логика ============
+def main():
+    print("Парсинг данных с сайта...")
+    posting_numbers = get_posting_numbers()
+    print("Получение данных из API...")
+    results = get_api_data(posting_numbers)
+
+    print("Сохранение в Excel...")
+    save_to_excel(results, OUTPUT_FILE)
+
+
+if __name__ == "__main__":
+    main()
